a56542262bee3ed6bcfef2268d5b628c5c975364
sge-babrams <ben.abrams@sleepygiant.com>
2014-09-18 14:17:09 -0700

    Update private-ipaddress.rb

--- files/default/plugins/private-ipaddress.rb
+++ files/default/plugins/private-ipaddress.rb
@@ -4,4 +4,4 @@ require_plugin 'network'
 
 addresses = network['interfaces'].map { |name, i| i['addresses'].keys }.flatten
 
-private_ipaddress addresses.grep(/^10\.|^172\.1[6-9]\.|^172\.2\d\.|^172\.3[0-1]\.|^192\.168/).first
+private_ipaddress addresses.grep(/^10\.|^172\.1[6-9]\.|^172\.2\d\.|^172\.3[0-1]\.|^192\.168/)
